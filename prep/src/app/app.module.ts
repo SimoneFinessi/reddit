2a4883f3cfdc37748503a0c736700e2b9412f7b3
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-02-01 13:53:30 +0000

    aggiunto componente

--- prep/src/app/app.module.ts
+++ prep/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
