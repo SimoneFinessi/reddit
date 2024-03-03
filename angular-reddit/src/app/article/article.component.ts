bfcf6d28e07d9e6273d94f8c31ba75439b13d666
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-03-03 16:32:42 +0000

    finito

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,22 +1,28 @@
-import { Component,HostBinding  } from '@angular/core';
+import { Component,HostBinding,Input  } from '@angular/core';
 import { Article } from './article.model'; //Non cancellare gli altri import
 
+@Component({
+  selector: 'app-article', //Il tag per inserire il componente sarà <app-article>
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+
 export class ArticleComponent {
-  @HostBinding('attr.class') cssClass = 'card';
-  article:Article
+  @HostBinding('attr.class') cssClass = 'card mt-4';
+  @Input() article:Article
 
   constructor() {
-    this.article = new Article('Angular 2','http://angular.io',10);
   }
 
   voteUp(): Boolean {
-    this.article.votes += 1; //accediamo alla proprietà votes di article
-    return false; //Non propagare l'evento
+    this.article.voteUp(); //Modificato qui 
+    return false;
   }
 
   voteDown():Boolean {
-    this.article.votes -= 1;
-    return false; //Non propagare l'evento
+    this.article.voteDown(); //Modificato qui
+    return false;
   }
+
   
 }
